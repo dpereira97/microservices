b08e63e04c7d0a59975e374cc6ed462780bdd279
Diogo Pereira <diogomiguel@outlook.com>
2025-08-03 23:16:40 +0100

    Added script to accelerate building and deploying images to Docker

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+cd accounts
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/accounts:$1
+cd ..
+cd loans
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/loans:$1
+cd ..
+cd cards
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/cards:$1
+cd ..
+cd configserver
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/configserver:$1
+cd ..
+cd eurekaserver
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/eurekaserver:$1
+cd ..
+cd gatewayserver
+mvn compile jib:dockerBuild
+docker image push docker.io/dpereira97/gatewayserver:$1
