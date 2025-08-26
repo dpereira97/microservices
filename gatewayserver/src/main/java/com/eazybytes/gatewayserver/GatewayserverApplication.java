a27741ec2faa4418b2280906dbb42dc5033a12a9
Diogo Pereira <30760434+dpereira97@users.noreply.github.com>
2025-08-26 21:58:43 +0100

    Event driven with Kafka

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -5,11 +5,9 @@ import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +29,7 @@ public class GatewayserverApplication {
 		return builder.routes()
 				.route(p -> p.path("/eazybank/accounts/**").filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}").addResponseHeader("X-Response-Time", LocalDateTime.now().toString()).circuitBreaker(config -> config.setName("accountsCircuitBreaker").setFallbackUri("forward:/contactSupport"))).uri("lb://ACCOUNTS"))
 				.route(p -> p.path("/eazybank/loans/**").filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}").addResponseHeader("X-Response-Time", LocalDateTime.now().toString()).retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET).setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))).uri("lb://LOANS"))
-				.route(p -> p.path("/eazybank/cards/**").filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}").addResponseHeader("X-Response-Time", LocalDateTime.now().toString()).requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver()))).uri("lb://CARDS"))
+//				.route(p -> p.path("/eazybank/cards/**").filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}").addResponseHeader("X-Response-Time", LocalDateTime.now().toString()).requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver()))).uri("lb://CARDS"))
 				.build();
 	}
 
@@ -43,10 +41,10 @@ public class GatewayserverApplication {
 		);
 	}
 
-	@Bean
-	public RedisRateLimiter redisRateLimiter() {
-		return new RedisRateLimiter(1, 1, 1);
-	}
+//	@Bean
+//	public RedisRateLimiter redisRateLimiter() {
+//		return new RedisRateLimiter(1, 1, 1);
+//	}
 
 	@Bean
 	KeyResolver keyResolver() {
